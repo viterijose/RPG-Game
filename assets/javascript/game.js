838b3589c31fa0ea136ebfcf4ab59ede63de6032
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-03-22 16:19:22 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,61 +13,84 @@ window.onload = function () {//window loads
     var characters = [
         goku = {
             name: "Goku",
-            image: "assets/images/goku.png",
+            image: ["assets/images/goku.png", "assets/images/goku_2.png", "assets/images/goku_3.png"],
             attack: 8,
             counter: 25,
-            health: 100,
-            chosen: "1"
+            health: 110,
+            chosen: 1
         },
         vegeta = {
             name: "Vegeta",
-            image: "assets/images/vegeta.png",
+            image: ["assets/images/vegeta.png", "assets/images/vegeta_2.png", "assets/images/vegeta_3.png"],
             attack: 8,
             counter: 25,
             health: 120,
-            chosen: "2"
+            chosen: 2
         },
         frieza = {
             name: "Frieza",
-            image: "assets/images/frieza.png",
+            image: ["assets/images/frieza.png", "assets/images/frieza_2.png", "assets/images/frieza_3.png"],
             attack: 8,
             counter: 25,
             health: 150,
-            chosen: "3"
+            chosen: 3
         },
         cell = {
             name: "Cell",
-            image: "assets/images/Cell.png",
+            image: ["assets/images/Cell.png", "assets/images/cell_2.png", "assets/images/cell_3.png"],
             attack: 8,
             counter: 25,
             health: 180,
-            chosen: "4"
+            chosen: 4
         }
     ]
+    var audio_transform = $("<audio class = transform>");
+    audio_transform.attr("src", "assets/javascript/Transform.mp3");
+    var audio_teleport = $("<audio class = teleport>");
+    audio_teleport.attr("src", "assets/javascript/Teleport.mp3");
+    var audio_punch = $("<audio class = punch>");
+    var audio_loose = $("<audio class = loose>");
+    audio_loose.attr("src", "assets/javascript/loose_life.mp3"); audio_punch.attr("src", "assets/javascript/Punch.mp3");
+    var audio_battle = $("<audio class = battle>");
+    audio_battle.attr("src", "assets/javascript/Battle_theme.mp3");
+    // audio_battle.play();
 
-    for (var i = 0; i < characters.length; i++) {
-        var newButton = $("<button>")
-        newButton.addClass("character_choose");
-        newButton.val(characters[i].health);
-        var newDiv = $("<div>");
-        newDiv.text(characters[i].name);
-        var newImg = $("<img height = 200px width = 170px>");
-        newImg.attr("src", characters[i].image);
-        var newFooter = $("<footer>");
-        newFooter.text("Health :");
-        var newSpan = $("<span>");
-        newSpan.addClass(characters[i].chosen);
-        newSpan.text(characters[i].health);
 
-        $(newButton).appendTo("#container_char");
-        $(newDiv).appendTo(newButton);
-        $(newImg).appendTo(newButton);
-        $(newSpan).appendTo(newFooter);
-        $(newFooter).appendTo(newButton);
+    function create() {
+
+        for (var i = 0; i < characters.length; i++) {
+            var newButton = $("<button>")
+            newButton.addClass("character_choose");
+            newButton.val(characters[i].health);
+            var newDiv = $("<div>");
+            newDiv.text(characters[i].name);
+            var newImg = $("<img height = 200px width = 170px>");
+            newImg.attr("src", characters[i].image[0]);
+            var newFooter = $("<footer>");
+            newFooter.text("Health :");
+            var newSpan = $("<span>");
+            newSpan.addClass(characters[i].chosen);
+            newSpan.text(characters[i].health);
+
+            $(newButton).appendTo("#container_char");
+            $(newDiv).appendTo(newButton);
+            $(newImg).appendTo(newButton);
+            $(newSpan).appendTo(newFooter);
+            $(newFooter).appendTo(newButton);
+        }
+
     }
+    create();
 
     function restart() {
+        $("#container_choose").empty();
+        $("#container_oponent").empty();
+        $("#container_char").empty();
+        create();
     }
+    $(".restart").on("click", function(){
+        restart();
+    })
     function gameover() {
         alert("YOU LOOSE!!");
     }
@@ -75,11 +98,38 @@ window.onload = function () {//window loads
         isOponentChosen = false;
         $("#container_oponent").empty();
         wins++;
-        if(wins == 1){ //get character image, erase src, put new src for transformation
-            $(".character_chosen").find("img").attr("src", "https://pre00.deviantart.net/502c/th/pre/f/2017/039/7/9/super_saiyan_goku_2_by_brusselthesaiyan-daydfdy.png");//make for every character
+        var char_chosen = $(".character_chosen").val();
+        audio_transform.play();
+
+        if (wins == 1) {
+
+            if (char_chosen == 110) {
+                $(".character_chosen").find("img").attr("src", characters[0].image[wins]);//make for every character
+            }
+            if (char_chosen == 120) {
+                $(".character_chosen").find("img").attr("src", characters[1].image[wins]);//make for every character
+            }
+            if (char_chosen == 150) {
+                $(".character_chosen").find("img").attr("src", characters[2].image[wins]);//make for every character
+            }
+            if (char_chosen == 180) {
+                $(".character_chosen").find("img").attr("src", characters[3].image[wins]);//make for every character
+            }
         }
-        if(wins == 2){
-            $(".character_chosen").find("img").attr("src", "https://vignette.wikia.nocookie.net/vsbattles/images/4/48/Goku_ssj2_by_spongeboss-d3np2hm.png/revision/latest?cb=20150428060108");
+        if (wins == 2) {
+
+            if (char_chosen == 110) {
+                $(".character_chosen").find("img").attr("src", characters[0].image[wins]);//make for every character
+            }
+            if (char_chosen == 120) {
+                $(".character_chosen").find("img").attr("src", characters[1].image[wins]);//make for every character
+            }
+            if (char_chosen == 150) {
+                $(".character_chosen").find("img").attr("src", characters[2].image[wins]);//make for every character
+            }
+            if (char_chosen == 180) {
+                $(".character_chosen").find("img").attr("src", characters[3].image[wins]);//make for every character
+            }
         }
         if (wins <= 1) {
             current_attack_power = 8 * count;
@@ -111,7 +161,7 @@ window.onload = function () {//window loads
                 $(this).addClass("character_oponent");
                 $(this).find("span").removeClass();
                 $(this).find("span").addClass("Oponent");
-                
+
                 isOponentChosen = true;
             }
         })
@@ -123,7 +173,7 @@ window.onload = function () {//window loads
 
         for (var k = 1; k <= count; k++) {
             if (wins == 0) {
-                attack_power = 8 * k; 
+                attack_power = 8 * k;
                 health_oponent = health_oponent - attack_power;
             } else {
                 attack_power = current_attack_power + 8 * k;
@@ -131,27 +181,27 @@ window.onload = function () {//window loads
             }
         }
         console.log("current health of oponent:" + health_oponent);
-        counter = 10 ;
+        counter = 10;
         console.log("health chosen:" + health_chosen);
         health_chosen = health_chosen - counter;
         console.log("health after counter:" + health_chosen);
         $(".Chosen").text(health_chosen);
         $(".Oponent").text(health_oponent);
 
-        if (health_chosen <= 0) {
-            gameover();
-        } else if (health_oponent <= 0) {
+        if (health_oponent <= 0) {
             alert("NEXT CHARACTER");
             next();
+        } else if (health_chosen <= 0) {
+            gameover();
         }
-        if (wins == 3){
+        if (wins == 3) {
             alert("YOU WON!!");
         }
     }
     $(".attack").on("click", function () {
-        if(isOponentChosen===true){
-        count++;
-        attack();
+        if (isOponentChosen === true) {
+            count++;
+            attack();
         }
     })
 
